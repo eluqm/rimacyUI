c4efa072293c6b6c5cdc980c49b5bcaa6357a88a
eluqm <edluquem@gmail.com>
2018-10-29 13:51:14 -0500

    29.10.2018

--- src/components/CobranzaRimacy.js
+++ src/components/CobranzaRimacy.js
@@ -1,27 +1,27 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
+
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormLabel from '@material-ui/core/FormLabel';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
 import FilledInput from '@material-ui/core/FilledInput';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Switch from '@material-ui/core/Switch';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
 const styles = theme=>({
 
     grow: {
@@ -33,8 +33,9 @@ const styles = theme=>({
 
     },
     root: {
-        display: 'flex',
-        flexWrap: 'wrap',
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
     },
     formControl: {
         margin: theme.spacing.unit,
@@ -48,16 +49,33 @@ const styles = theme=>({
         marginRight: theme.spacing.unit,
         width: 200,
     },
+    group: {
+        margin: `${theme.spacing.unit}px 0`,
+    },
 });
 
+let names = [
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
 
+];
 const options = [
     '',
     '7 dias',
     '15 dias',
 
 ];
-
+function getStyles(name, that) {
+    return {
+        fontWeight:
+            that.state.name.indexOf(name) === -1
+                ? that.props.theme.typography.fontWeightRegular
+                : that.props.theme.typography.fontWeightMedium,
+    };
+}
 class CobranzaRimacy extends React.Component {
     state = {
         date : new Date(),
@@ -70,6 +88,20 @@ class CobranzaRimacy extends React.Component {
         _selectedZona : "",
         _anchorEl: null,
         _selectedIndex: 1,
+        labelWidth: 0,
+        _week: true,
+        _monthly: false,
+        checkedA:true,
+        checkedB:true,
+        commentInput: '',
+        valueday:0,
+        _lunes:false,
+        _martes:false,
+        _miercoles:false,
+        _jueves:false,
+        _viernes:false,
+        _totaldaysvalue:1,
+        //antoine: true,
     };
 //menu list
     //-------------------------------------------------------------
@@ -112,6 +144,32 @@ class CobranzaRimacy extends React.Component {
         })
 
     }
+
+    handleChangeFreq = xstate => event => {
+        this.setState({ [xstate[0]]: event.target.checked });
+        //this.setState({_week:})
+        console.log(this.state._week);
+        console.log(this.state._monthly);
+    };
+    handleChangeInput = commnent => event => {
+        this.setState({
+            [commnent]: event.target.value,
+        });
+    };
+
+    handleChangeWeek = name => event => {
+        this.setState({ [name]: event.target.checked });
+        console.log(event.target.checked);
+        console.log(event.target.value);
+        if(event.target.checked==true)
+        {
+            this.setState({_totaldaysvalue:this.state._totaldaysvalue*parseInt(event.target.value)})
+        }else{this.setState({_totaldaysvalue:this.state._totaldaysvalue/parseInt(event.target.value)})}
+        //console.log(event.target.value);
+        //console.log(event.target.checked);
+
+    };
+
     componentDidMount() {
         fetch("http://localhost:8181/v1/allempl")
             .then((response) => {
@@ -124,7 +182,7 @@ class CobranzaRimacy extends React.Component {
         }).catch(error => {
             console.log(error);
         });
-
+       // labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWid
         fetch("http://localhost:8181/v1/allzones")
             .then((response) => {
                 return response.json();
@@ -148,82 +206,64 @@ class CobranzaRimacy extends React.Component {
         const open = Boolean(anchorEl);
 
         return (
-            <div className={classes.root}>
-                <Divider />
-                <FormGroup>
-                    <FormControlLabel
-                        control={
-                            <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />
-                        }
-                        label={auth ? 'Logout' : 'Login'}
-                    />
-                </FormGroup>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" color="inherit" className={classes.grow}>
-                            Cobranza
-                        </Typography>
-                        {auth && (
-                            <div>
-                                <IconButton
-                                    aria-owns={open ? 'menu-appbar' : null}
-                                    aria-haspopup="true"
-                                    onClick={this.handleMenu}
-                                    color="inherit"
-                                >
-                                    <AccountCircle />
-                                </IconButton>
-                                {/*<Menu
-                                    id="menu-appbar"
-                                    anchorEl={anchorEl}
-                                    anchorOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    open={open}
-                                    onClose={this.handleClose}
-                                >
-                                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                                </Menu>*/}
-
-                            </div>
 
 
-                        )}
 
-                    </Toolbar>
+                <Paper className={classes.root} elevation={1}>
 
-                        <FormControl variant="filled" className={classes.formControl}>
 
-                            <InputLabel htmlFor="filled-age-simple">Nombre</InputLabel>
+
+
+
+                    <FormControl variant="filled" className={classes.formControl}>
+
+                            <InputLabel htmlFor="filled-age-simple">Empleado</InputLabel>
                             <Select value={this.state.selectedTeam}
                                     onChange={(e) => this.setState({selectedTeam: e.target.value, validationError: e.target.value === "" ? "You must select your favourite team" : ""})}>
                                 {this.state.empls.map((team) => <MenuItem key={team.value} value={team.value}>{team.display}</MenuItem>)}
                                 input={<FilledInput name="Seleccione Nombre" id="filled-age-simple" />}
                             </Select>
-                        </FormControl>
-
+                    </FormControl>
 
                     <form className={classes.container} noValidate autoComplete="off">
-                    <TextField
-                        id="date"
-                        label="Frecuencia"
-                        type="date"
-                        defaultValue={this.state.date}
-                        className={classes.textField}
-                        InputLabelProps={{
+                        <TextField
+                            id="date"
+                            label="Frecuencia"
+                            type="date"
+                            defaultValue={this.state.date}
+                            className={classes.textField}
+                            InputLabelProps={{
                             shrink: true,
                         }}
-                    />
-                        <List component="nav">
+                        />
+                    </form>
+                    <FormGroup row>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={this.state._week}
+                                    onChange={this.handleChangeFreq(['_week','_monthly'])}
+                                    value="_week"
+                                />
+                            }
+                            label="semanal"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={this.state._monthly}
+                                    onChange={this.handleChangeFreq(['_monthly','_week'])}
+                                    value="_monthly"
+                                    //color="Secondary"
+                                />
+                            }
+                            label="quincenal"
+                        />
+
+                    </FormGroup>
+
+
+                    {/*<List component="nav">
                             <ListItem
                                 button
                                 aria-haspopup="true"
@@ -253,19 +293,114 @@ class CobranzaRimacy extends React.Component {
                                     {option}
                                 </MenuItem>
                             ))}
-                        </Menu>
-                    </form>
-                    <FormControl variant="filled" className={classes.formControl}>
+                        </Menu>*/}
 
-                        <InputLabel htmlFor="filled-age-simple">Zona</InputLabel>
-                        <Select value={this.state._selectedZona}
+
+
+
+                        <FormControl variant="outlined" className={classes.formControl}>
+
+                            <InputLabel
+                                ref={ref => {
+                                    this.InputLabelRef = ref;
+                                }}
+                                htmlFor="outlined-age-simple"
+                            >
+                                Zona
+                            </InputLabel>
+                            <Select value={this.state._selectedZona}
                                 onChange={(e) => this.setState({_selectedZona: e.target.value, validationError: e.target.value === "" ? "You must select your favourite team" : ""})}>
-                            {this.state._zona.map((team) => <MenuItem key={team.value} value={team.value}>{team.display}</MenuItem>)}
-                            input={<FilledInput name="Seleccione Zona" id="filled-age-simple" />}
-                        </Select>
+                                {this.state._zona.map((team) => <MenuItem key={team.value} value={team.value}>{team.display}</MenuItem>)}
+                                input={
+                                    <OutlinedInput
+                                        labelWidth={this.state.labelWidth}
+                                        name="Zona"
+                                        id="outlined-age-simple"
+                                    />}
+                            </Select>
+
+                        </FormControl>
+                    <FormControl component="fieldset" className={classes.formControl}>
+                        <FormLabel component="legend">Dias</FormLabel>
+
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={this.state._lunes}
+                                    onChange={this.handleChangeWeek('_lunes')}
+                                    value='3'
+                                />
+                            }
+                            label="Lunes"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={this.state._martes}
+                                    onChange={this.handleChangeWeek('_martes')}
+                                    value='5'
+                                />
+                            }
+                            label="Martes"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={this.state._miercoles}
+                                    onChange={this.handleChangeWeek('_miercoles')}
+                                    value='7'
+                                />
+                            }
+                            label="Miercoles"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={this.state._jueves}
+                                    onChange={this.handleChangeWeek('_jueves')}
+                                    value='11'
+                                />
+                            }
+                            label="Jueves"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={this.state._viernes}
+                                    onChange={this.handleChangeWeek('_viernes')}
+                                    value='13'
+                                />
+                            }
+                            label="Viernes"
+                        />
+
+
+
                     </FormControl>
-                </AppBar>
-            </div>
+
+                        <TextField
+                            id="outlined-name"
+                            label="Comentarios"
+                            className={classes.textField}
+                            value={this.state.commentInput}
+                            onChange={this.handleChangeInput('commnentInput')}
+                            margin="normal"
+                            variant="outlined"
+                        />
+
+
+
+
+                    <Button variant="outlined" onClick={this.handleClick} color="primary" className={classes.button}>
+                        Asignar Zona
+                        <p>{this.state._totaldaysvalue}</p>
+                    </Button>
+
+
+    </Paper>
+
+
+
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
 import './index.css';
 //import Table from './TableEmpl';
@@ -8,13 +14,78 @@ import PickerRimacy from './components/PickerRimacy';
 import InputRimacy from './components/InputRimacy';
 import MultiSelectRimacy from './components/MultiSelectRimacy';
 import CobranzaRimacy from './components/CobranzaRimacy';
+import SwitchLabels from './components/SwitchLabels';
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
+    );
+}
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+});
 
 class App extends Component{
+    state = {
+        value: 0,
+    };
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
     render() {
+        const { classes } = this.props;
+        const { value } = this.state;
+
         return (
             <div className="App">
-                <h1>Crear Zona</h1>
+                <AppBar position="static" color="default">
+                    <Tabs
+                        value={value}
+                        onChange={this.handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        scrollable
+                        scrollButtons="auto"
+                    >
+                        <Tab label="Crear Zona" />
+                        <Tab label="Asignar Ruta" />
+                        <Tab label="Item Four" />
+                        {/*}  <Tab label="Item Three" />
+                        <Tab label="Item Four" />
+                        <Tab label="Item Five" />
+                        <Tab label="Item Six" />
+                        <Tab label="Item Seven" />*/}
+                    </Tabs>
+                </AppBar>
+                {value === 0 && <TabContainer>
+                    {/*ReactDOM.render(<MultiSelectRimacy/>, document.getElementById('multiRuta'));
+                    */}
+                    <MultiSelectRimacy/>
+                    </TabContainer>}
+                {value === 1 && <TabContainer>
+                    {/*ReactDOM.render(, document.getElementById('cobranza'));</TabContainer>}
+                    */}
+                    <CobranzaRimacy />
+                </TabContainer>
 
+                }
+                {value === 2 && <TabContainer>
+                    <SwitchLabels/>
+                </TabContainer>
+                }
+                {/* {value === 3 && <TabContainer>Item Four</TabContainer>}
+                {value === 4 && <TabContainer>Item Five</TabContainer>}
+                {value === 5 && <TabContainer>Item Six</TabContainer>}
+                {value === 6 && <TabContainer>Item Seven</TabContainer>}*/}
             </div>
         );
     }
@@ -27,8 +98,8 @@ class App extends Component{
 //ReactDOM.render(<EmpList />, document.getElementById('empleList'));
 //ReactDOM.render(<PickerRimacy />, document.getElementById('pickerdate'));
 //ReactDOM.render(<InputRimacy />, document.getElementById('inputZona'));
-ReactDOM.render(<MultiSelectRimacy />, document.getElementById('multiRuta'));
-ReactDOM.render(<CobranzaRimacy />, document.getElementById('cobranza'));
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

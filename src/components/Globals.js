fffb22ebf6ee96b5229d30b24ea7053d0d9383ae
eluqm <edluquem@gmail.com>
2018-12-19 12:43:58 -0500

    servername

--- src/components/Globals.js
+++ src/components/Globals.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default class Globals extends React.Component {
 
     state = {
-        hostname:"http://localhost:8181/",
+        //hostname:"http://localhost:8181/",
+        hostname:"http://174.138.48.60:8080/rimacy/",
     }
 }
